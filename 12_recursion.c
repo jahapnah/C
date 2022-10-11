303b8ee6f5a5553d8359ff532eafbb09261c51f8
Rishi Kant <110294979+jahapnah@users.noreply.github.com>
2022-10-11 13:04:42 +0530

    Update 12_recursion.c

--- 12_recursion.c
+++ 12_recursion.c
@@ -1,3 +1,4 @@
+// Make a program of factorial of a given number using recursion
 #include<stdio.h>
 int factorial(int y);
 
@@ -18,3 +19,48 @@ int factorial(int y){
         return (factorial(y-1))*y;
     }
 }
+
+
+
+//Make a fibonacci series using recursion
+
+#include <stdio.h>
+#include <conio.h>
+
+int fibonacci(int x);
+
+int main()
+{
+    int in;
+    printf("Enter the value for fibonacci :\n");
+    scanf("%d", &in);
+
+
+    for (int i = 0; i < in; i++)
+    {
+        printf("%d \n", fibonacci(i));
+    }
+    return 0;
+}
+
+int fibonacci(int x)
+{
+    if (x == 0)
+    {
+        return 0;
+    }
+    else if (x == 1)
+    {
+        return 1;
+    }
+
+    else
+    {
+        return fibonacci(x - 1) + fibonacci(x - 2);
+    }
+}
+
+
+
+
+
